8e0e29879d51dc90cedfb11b9a8d3881b5e4db63
ankit-sikarwar-001 <ankitsikarwar681@gmail.com>
2024-06-23 15:22:06 +0530

    api edited and exchange rates start displaying

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
-const BASE_URL =
-  "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
+const BASE_URL = "https://2024-03-06.currency-api.pages.dev/v1/currencies";
 let dropdowns = document.querySelectorAll(".dropdown select");
+let btn = document.querySelector("form button");
+let amount = document.querySelector(".amount input");
+let fromcurr = document.querySelector(".from select");
+let tocurr = document.querySelector(".to select");
+let msg = document.querySelector("#msg");
 
 for (let select of dropdowns) {
   for (const currcode in countryList) {
@@ -23,3 +27,20 @@ let updateflag = (evt, img) => {
   let currcode = countryList[evt.value];
   img.src = `https://flagsapi.com/${currcode}/flat/64.png`;
 };
+
+btn.addEventListener("click", async (evt) => {
+  evt.preventDefault();
+  let amtvalue = amount.value;
+  if (amtvalue < 0 || amtvalue == "") {
+    amount.value = 1;
+  }
+  const URL = `${BASE_URL}/${fromcurr.value.toLowerCase()}.json`;
+  let response = await fetch(URL);
+  let read = await response.json();
+  let data = read[fromcurr.value.toLowerCase()];
+  // console.log(fromcurr.value.toLowerCase(), tocurr.value);
+  console.log(data[tocurr.value.toLowerCase()]);
+  msg.innerText = `1 ${fromcurr.value.toUpperCase()} = ${
+    data[tocurr.value.toLowerCase()]
+  } ${tocurr.value.toUpperCase()}`;
+});
