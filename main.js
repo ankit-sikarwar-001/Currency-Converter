5cbefe636b9a3dbe8e72c8e99d24ed2299a18ca2
ankit-sikarwar-001 <ankitsikarwar681@gmail.com>
2024-06-23 10:56:43 +0530

    structure is ready

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 const url = "https://flagsapi.com/IN/flat/64.png";
+let select = document.querySelectorAll("option");
 let option = document.querySelectorAll(".select");
+let img = document.querySelectorAll("img");
 
 for (const ad of option) {
   for (const op in countryList) {
     let newop = document.createElement("option");
-    // console.log(op);
     newop.value = op;
     newop.innerText = countryList[op];
     ad.appendChild(newop);
