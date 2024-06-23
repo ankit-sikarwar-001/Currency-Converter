45499791cffd24fdd54ac482e34b1cd4c0e2d933
ankit-sikarwar-001 <ankitsikarwar681@gmail.com>
2024-06-23 14:01:20 +0530

    structure is ready

--- main.js
+++ main.js
@@ -1,13 +1,25 @@
-const url = "https://flagsapi.com/IN/flat/64.png";
-let select = document.querySelectorAll("option");
-let option = document.querySelectorAll(".select");
-let img = document.querySelectorAll("img");
+const BASE_URL =
+  "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
+let dropdowns = document.querySelectorAll(".dropdown select");
 
-for (const ad of option) {
-  for (const op in countryList) {
+for (let select of dropdowns) {
+  for (const currcode in countryList) {
     let newop = document.createElement("option");
-    newop.value = op;
-    newop.innerText = countryList[op];
-    ad.appendChild(newop);
+    newop.innerText = currcode;
+    newop.value = currcode;
+    if (select.name == "from" && newop.value == "USD") {
+      newop.selected = "selected";
+    } else if (select.name == "to" && newop.value == "INR") {
+      newop.selected = "selected";
+    }
+    select.appendChild(newop);
+    select.addEventListener("change", (element) => {
+      let elem = select.parentElement.querySelector("img");
+      updateflag(element.target, elem);
+    });
   }
 }
+let updateflag = (evt, img) => {
+  let currcode = countryList[evt.value];
+  img.src = `https://flagsapi.com/${currcode}/flat/64.png`;
+};
